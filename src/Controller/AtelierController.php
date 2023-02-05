d307fd08a4c5e76c14a6de2e18c069e099791a16
The_ChaosStar <caron.donovan1@outlook.fr>
2023-02-05 17:00:46 +0100

    update

--- src/Controller/AtelierController.php
+++ src/Controller/AtelierController.php
@@ -4,40 +4,101 @@ namespace App\Controller;
 
 use App\Entity\Atelier;
 use App\Repository\AtelierRepository;
-use App\Repository\EventRepository;
+use App\Service\VersioningService;
 use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
-use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class AtelierController extends AbstractController
 {
+    /**
+     *  @OA\Response(
+     *      response=200,
+     *      description="Retourne la liste des ateliers",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Atelier::class, groups={"getAteliers"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Atelier")
+     * 
+     * @param AtelierRepository $atelierRepository
+     * @param SerializerInterface $serializer
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
     #[Route('/api/ateliers', name: 'ateliers', methods: ['GET'])]
     public function getAteliers(
         AtelierRepository $atelierRepository,
-        SerializerInterface $serializer
+        SerializerInterface $serializer,
+        VersioningService $versioningService
     ): JsonResponse {
+        
         $atelier = $atelierRepository->findAll();
-        $jsonAtelier = $serializer->serialize($atelier, "json", ["groups" => "getAteliers"]);
-
+        $version = $versioningService->getVersion();
+        $context = SerializationContext::create()->setGroups(["getAteliers"]);
+        $context->setVersion($version);
+        $jsonAtelier = $serializer->serialize($atelier, "json", $context);
+        
         return new JsonResponse($jsonAtelier, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="Retourne un atelier avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Atelier::class, groups={"getAteliers"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Atelier")
+     * 
+     * @param Atelier $atelier, 
+     * @param SerializerInterface $serializer, 
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
     #[Route('/api/atelier/{id}', name: 'atelier', methods: ['GET'])]
     public function getEvent(
         Atelier $atelier,
-        SerializerInterface $serializer
+        SerializerInterface $serializer,
+        VersioningService $versioningService
     ): JsonResponse {
-        $jsonAtelier = $serializer->serialize($atelier, "json", ["groups" => "getAteliers"]);
+        $version = $versioningService->getVersion(); 
+        $context = SerializationContext::create()->setGroups(["getAteliers"]);
+        $context->setVersion($version);
+        $jsonAtelier = $serializer->serialize($atelier, "json", $context);
         return new JsonResponse($jsonAtelier, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="supprime un ateliers avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Atelier::class, groups={"getAteliers"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Atelier")
+     *  @param Atelier $atelier, 
+     *  @param EntityManagerInterface $em
+     *  @return JsonResponse
+     */
     #[Route('/api/atelier/{id}', name: 'delete_atelier', methods: ['DELETE'])]
     public function deleteEvent(
         Atelier $atelier, 
@@ -50,6 +111,25 @@ class AtelierController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="créer un atelier",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Atelier::class, groups={"getAteliers"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Atelier")
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
     #[Route('/api/atelier', name: 'create_atelier', methods: ['POST'])]
     public function createEvent(
         Request $request,
@@ -69,23 +149,41 @@ class AtelierController extends AbstractController
         $em->persist($atelier);
         $em->flush();
 
-        $jsonAtelier = $serializer->serialize($atelier, 'json', ['groups' => 'getAteliers']);
+        $context = SerializationContext::create()->setGroups(["getAteliers"]);
+        $jsonAtelier = $serializer->serialize($atelier, 'json', $context);
 
         $location = $urlGenerator->generate('atelier', ['id' => $atelier->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
 
         return new JsonResponse($jsonAtelier, Response::HTTP_CREATED, ["Location" => $location], true);
     }
 
-    #[Route('/api/atelier', name: 'update_atelier', methods: ['PUT'])]
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="met a jour un atelier avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Atelier::class, groups={"getAteliers"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Atelier")
+     * 
+     * @param Request $request, 
+     * @param SerializerInterface $serializer, 
+     * @param EntityManagerInterface $em, 
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
+    #[Route('/api/atelier/{id}', name: 'update_atelier', methods: ['PUT'])]
     public function updateAtelier(
         Request $request,
         SerializerInterface $serializer,
-        Atelier $currentAtelier,
         EntityManagerInterface $em,
         ValidatorInterface $validator
     ) : JsonResponse {
 
-        $updatedAtelier = $serializer->deserialize($request->getContent(), Atelier::class, 'json', [AbstractNormalizer::OBJECT_TO_POPULATE => $currentAtelier]);
+        $updatedAtelier = $serializer->deserialize($request->getContent(), Atelier::class, 'json');
 
         $errors = $validator->validate($updatedAtelier);
         if ($errors->count() > 0)
@@ -97,6 +195,4 @@ class AtelierController extends AbstractController
 
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
-
-
 }
--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Event;
 use App\Repository\AtelierRepository;
 use App\Repository\EventRepository;
+use App\Service\VersioningService;
 use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
@@ -18,15 +19,52 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use OpenApi\Annotations as OA;
 
 class EventController extends AbstractController
 {
+    /**
+     *  @OA\Response(
+     *      response=200,
+     *      description="Retourne la liste des évènements",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Event::class, groups={"getEvents"}))
+     *      )
+     *  )
+     *
+     *  @OA\Parameter(
+     *     name="page",
+     *     in="query",
+     *     description="La page que l'on veut récupérer",
+     *     @OA\Schema(type="int") 
+     *  )
+     * 
+     *  @OA\Parameter(
+     *     name="limit",
+     *     in="query",
+     *     description="Le nombre d'éléments que l'on veut récupérer",
+     *     @OA\Schema(type="int")
+     *  )
+     *
+     *  @OA\Tag(name="Events")
+     * 
+     * @param EventRepository $eventRepository
+     * @param SerializerInterface $serializer
+     * @param Request $request
+     * @param TagAwareCacheInterface $cache
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
     #[Route('/api/events', name: 'events', methods: ['GET'])]
     public function getEvents(
         EventRepository $eventRepository,
         SerializerInterface $serializer,
         Request $request,
-        TagAwareCacheInterface $cache
+        TagAwareCacheInterface $cache,
+        VersioningService $versioningService
     ): JsonResponse {
 
         $page = $request->get('page', 1);
@@ -34,26 +72,65 @@ class EventController extends AbstractController
 
         $idCache = "getAllEvents-" . $page . "-" . $limit;
 
-        $jsonEventList = $cache->get($idCache, function (ItemInterface $item) use ($eventRepository, $page, $limit, $serializer) {
+        $jsonEventList = $cache->get($idCache, function (ItemInterface $item) use ($eventRepository, $page, $limit, $serializer, $versioningService) {
             $item->tag("eventCache");
             $eventList = $eventRepository->findAllWithPagination($page, $limit);
+            $version = $versioningService->getVersion();
             $context = SerializationContext::create()->setGroups(["getEvents"]);
+            $context->setVersion($version);
             return $serializer->serialize($eventList, "json", $context);
         });
 
         return new JsonResponse($jsonEventList, Response::HTTP_OK, [], true);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="Retourne un évènement avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Event::class, groups={"getEvents"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Events")
+     * 
+     * @param Event $event,
+     * @param SerializerInterface $serializer,
+     * @param VersioningService $versioningService
+     * @return JsonResponse
+     */
     #[Route('/api/event/{id}', name: 'event', methods: ['GET'])]
     public function getEvent(
         Event $event,
-        SerializerInterface $serializer
+        SerializerInterface $serializer,
+        VersioningService $versioningService
     ): JsonResponse {
+        $version = $versioningService->getVersion();
         $context = SerializationContext::create()->setGroups(["getEvents"]);
+        $context->setVersion($version);
         $jsonEvent = $serializer->serialize($event, "json", $context);
         return new JsonResponse($jsonEvent, Response::HTTP_OK, ['accept' => 'json'], true);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="supprime un évènement avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Event::class, groups={"getEvents"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Events")
+     * 
+     * @param Event $event, 
+     * @param EntityManagerInterface $em, 
+     * @param TagAwareCacheInterface $cachePool
+     * @return JsonResponse
+     */
     #[Route('/api/event/{id}', name: 'delete_event', methods: ['DELETE'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour creer un evenement')]
     public function deleteEvent(
@@ -68,6 +145,26 @@ class EventController extends AbstractController
         return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="créer un évènement",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Event::class, groups={"getEvents"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Events")
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param EntityManagerInterface $em
+     * @param UrlGeneratorInterface $urlGenerator
+     * @param AtelierRepository $atelierRepository
+     * @param ValidatorInterface $validator
+     * @return JsonResponse
+     */
     #[Route('/api/event', name: 'create_event', methods: ['POST'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour creer un evenement')]
     public function createEvent(
@@ -77,20 +174,20 @@ class EventController extends AbstractController
         UrlGeneratorInterface $urlGenerator,
         AtelierRepository $atelierRepository,
         ValidatorInterface $validator
-    ) : JsonResponse {
-        
+    ): JsonResponse {
+
         $event = $serializer->deserialize($request->getContent(), Event::class, 'json');
-        
-        
+
+
         $errors = $validator->validate($event);
         if ($errors->count() > 0)
             return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
-           
+
         $content = $request->toArray();
         $idAtelier = $content["idAtelier"] ?? -1;
-        
+
         $event->setAtelier($atelierRepository->find($idAtelier));
-    
+
         $em->persist($event);
         $em->flush();
 
@@ -101,8 +198,29 @@ class EventController extends AbstractController
 
         return new JsonResponse($jsonEvent, Response::HTTP_CREATED, ["Location" => $location], true);
     }
-    
-    #[Route('/api/event', name: 'update_event', methods: ['PUT'])]
+
+    /**
+     * @OA\Response(
+     *      response=200,
+     *      description="met a jour un évènement avec l'id",
+     *      @OA\JsonContent(
+     *          type="array",
+     *          @OA\Items(ref=@Model(type=Event::class, groups={"getEvents"}))
+     *      )
+     *  )
+     * 
+     *  @OA\Tag(name="Events")
+     *
+     * @param Request $request
+     * @param SerializerInterface $serializer
+     * @param Event $currentEvent
+     * @param EntityManagerInterface $em
+     * @param AtelierRepository $atelierRepository
+     * @param ValidatorInterface $validator
+     * @param TagAwareCacheInterface $cache
+     * @return JsonResponse
+     */
+    #[Route('/api/event/{id}', name: 'update_event', methods: ['PUT'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour creer un evenement')]
     public function updateEvent(
         Request $request,
@@ -112,14 +230,14 @@ class EventController extends AbstractController
         AtelierRepository $atelierRepository,
         ValidatorInterface $validator,
         TagAwareCacheInterface $cache
-    ) : JsonResponse {
+    ): JsonResponse {
 
         $updatedEvent = $serializer->deserialize($request->getContent(), Event::class, 'json');
-        
+
         $errors = $validator->validate($updatedEvent);
         if ($errors->count() > 0)
             return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
-        
+
         $content = $request->toArray();
         $idAtelier = $content['idAtelier'] ?? -1;
 
@@ -132,5 +250,4 @@ class EventController extends AbstractController
 
         return new JsonResponse(null, JsonResponse::HTTP_NO_CONTENT);
     }
-
 }
